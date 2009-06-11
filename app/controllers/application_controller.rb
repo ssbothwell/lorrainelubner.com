242ab5dda8fa3d418a46a5ed1f89e3175e14cabf
Solomon Bothwell <solomon@yellowshark.gateway.2wire.net>
2009-06-10 23:08:17 -0700

    maintenance_mode.rb for web:disable fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
+  include MaintenanceMode
+  before_filter :disabled?
   helper :all # include all helpers, all the time
 
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
