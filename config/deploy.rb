52c754e45f0531bd8cd2e5c811e12a17d4a76e09
Solomon Bothwell <solomon@yellowshark.local>
2009-06-27 13:23:54 -0700

    working dev

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,13 +22,26 @@ set :scm_passphrase, "haskard"
 ssh_options[:paranoid] = false
 default_run_options[:pty] = true
 
-# Passenger
+# Passenger Integration
 
-namespace :passenger do
-  desc "Restart Application"
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
   end
+
+  desc "Symlink shared configs and folders on each release."  
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"    
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"    
+  end
+  
+  desc "Sync the public/assets directory."
+  task :assets do
+    system "rsync -vr --exclude='.DS_Store' public/assets #{user}@thetis.dreamhost.com:#{shared_path}/"
+    system "rsync -vr --exclude='.DS_Store' db/production.sqlite3 #{user}@thetis.dreamhost.com:#{shared_path}/"
+  end
 end
 
-after :deploy, "passenger:restart"
+after 'deploy:update_code', 'deploy:symlink_shared'
